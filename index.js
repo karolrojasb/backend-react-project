0363dd30f220aeaff65313187155ccf65d1d5b72
karolrojas07 <karol.rojasb.kommit@gmail.com>
2022-04-30 23:22:03 -0500

    Fix moongose conection to localhost error

--- index.js
+++ index.js
@@ -4,18 +4,16 @@ const PORT_SERVER = process.env.PORT || 3977
 const { API_VERSION, IP_SERVER, PORT_DB} = require('./config')
 
 mongoose.connect(
-  `mongodb://${IP_SERVER}:${PORT_DB}/proyect_db`,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err, res) => {
-    if (err) throw err
-    else {
-      console.log(' Success connection to DB ')
-      APP.listen(PORT_SERVER, () => {
-        console.log('########################')
-        console.log('####### API REST  ######')
-        console.log('########################')
-        console.log(`http://${IP_SERVER}:${PORT_SERVER}/api/${API_VERSION}/`)
-      })
-    }
-  }
-)
+  `mongodb://0.0.0.0:${PORT_DB}/project_db`,
+  { useNewUrlParser: true, useUnifiedTopology: false }
+).then(()=>{
+  console.log(' Success connection to DB ')
+  APP.listen(PORT_SERVER, () => {
+    console.log('########################')
+    console.log('####### API REST  ######')
+    console.log('########################')
+    console.log(`http://${IP_SERVER}:${PORT_SERVER}/api/${API_VERSION}/`)
+  })
+}).catch(err => {
+  console.dir(err)
+})
