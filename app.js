8bd70de1f6b8fb0831e35da5bf05c6eee439862b
karolrojas07 <karol.rojasb.kommit@gmail.com>
2022-05-21 19:42:12 -0500

    Guide 6

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const bodyparser = require('body-parser')
+const cors = require('cors')
 const APP = express()
 const { API_VERSION } = require('./config')
 
 APP.use(bodyparser.urlencoded({extended: false}))
+APP.use(cors())
 APP.use(bodyparser.json())
 
 module.exports = APP
